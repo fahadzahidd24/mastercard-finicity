5d09bb160a7d9d7bce1104028dc798c8f6d0bd8a
unknown <fahadzahidd24@gmail.com>
2023-09-18 22:07:20 +0500

    fix

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get('/', (req, resp) => {
     }).then(async res2 => {
         console.log("res2", res2);
         token = res2.token;
-        return fetch("https://api.finicity.com/aggregation/v2/customers/testing", {
+        return fetch("https://api.finicity.com/aggregation/v2/customers/active", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
