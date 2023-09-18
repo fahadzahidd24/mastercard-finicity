a201dd30d86ab8316a75940c368bd91232cfaf00
unknown <fahadzahidd24@gmail.com>
2023-09-18 18:11:29 +0500

    refresh Accounts

--- index.js
+++ index.js
@@ -5,77 +5,106 @@ const cors = require('cors');
 
 app.use(cors());
 
+app.post('/refreshAccounts', (req, res) => {
+    const { customerId, token, appKey } = req.body;
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append("Finicity-App-Token", token);
+    myHeaders.append("Finicity-App-Key", appKey);
 
+    var raw = JSON.stringify({});
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+
+    fetch(`https://api.finicity.com/aggregation/v1/customers/${customerId}/accounts`, requestOptions)
+        .then(response => response.json())
+        .then(result => res.json({
+            data: result
+        }))
+        .catch(error => {
+            console.log('error', error);
+            res.status(400).json({
+                error: error
+            })
+        });
+})
 
 app.get('/', (req, resp) => {
-let token,customerId;
-fetch("https://api.finicity.com/aggregation/v2/partners/authentication", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "Finicity-App-Key": "64fbfd97b243ce354d5211e2283b340d",
-        "Accept": "application/json",
-    },
-    body: JSON.stringify({
-        "partnerId": "2445584284357",
-        "partnerSecret": "wNB8rKJWoL7TkkbtUdqA"
-    })
-}).then(res => {
-    return res.json();
-}).then(async res2 => {
-    console.log("res2", res2);
-    token = res2.token;
-    return fetch("https://api.finicity.com/aggregation/v2/customers/testing", {
+    let token, customerId;
+    fetch("https://api.finicity.com/aggregation/v2/partners/authentication", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "Accept": "application/json",
             "Finicity-App-Key": "64fbfd97b243ce354d5211e2283b340d",
-            "Finicity-App-Token": res2.token,
+            "Accept": "application/json",
         },
         body: JSON.stringify({
-            "username": `johnnn${Math.random() * 100000}ssddaaqq`,
-            "firstName": "John",
-            "lastName": "Doe",
+            "partnerId": "2445584284357",
+            "partnerSecret": "wNB8rKJWoL7TkkbtUdqA"
         })
-    }).then(res3 => {
-        console.log(res3);
-        return res3.json();
-    }).then(async res4 => {
-        console.log("res4 ==>", res4);
-        customerId = res4.id;
-        return fetch("https://api.finicity.com/connect/v2/generate", {
+    }).then(res => {
+        return res.json();
+    }).then(async res2 => {
+        console.log("res2", res2);
+        token = res2.token;
+        return fetch("https://api.finicity.com/aggregation/v2/customers/testing", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json",
                 "Finicity-App-Key": "64fbfd97b243ce354d5211e2283b340d",
-                "Finicity-App-Token": res2.token
+                "Finicity-App-Token": res2.token,
             },
             body: JSON.stringify({
-                "partnerId": "2445584284357",
-                "customerId": res4.id,
+                "username": `johnnn${Math.random() * 100000}ssddaaqq`,
+                "firstName": "John",
+                "lastName": "Doe",
             })
-        }).then(res5 => {
-            return res5.json();
-        }).then(async res6 => {
-            console.log(res6);
-            return resp.status(200).json({
-                link : res6.link,
-                customerId : customerId,
-                token : token,
-                appKey: "64fbfd97b243ce354d5211e2283b340d"
+        }).then(res3 => {
+            console.log(res3);
+            return res3.json();
+        }).then(async res4 => {
+            console.log("res4 ==>", res4);
+            customerId = res4.id;
+            return fetch("https://api.finicity.com/connect/v2/generate", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Finicity-App-Key": "64fbfd97b243ce354d5211e2283b340d",
+                    "Finicity-App-Token": res2.token
+                },
+                body: JSON.stringify({
+                    "partnerId": "2445584284357",
+                    "customerId": res4.id,
+                })
+            }).then(res5 => {
+                return res5.json();
+            }).then(async res6 => {
+                console.log(res6);
+                return resp.status(200).json({
+                    link: res6.link,
+                    customerId: customerId,
+                    token: token,
+                    appKey: "64fbfd97b243ce354d5211e2283b340d"
+                })
+
+            }).catch(err => {
+                console.log("error:", err)
             })
-            
         }).catch(err => {
             console.log("error:", err)
         })
     }).catch(err => {
         console.log("error:", err)
     })
-}).catch(err => {
-    console.log("error:", err)
-})
 
 });
 
