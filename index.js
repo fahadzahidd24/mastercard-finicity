8ed0d81c79f8509336d255743dd46b61ed8c415f
Fahad Zahid <80856274+fahadzahidd24@users.noreply.github.com>
2024-03-22 17:33:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.post('/customer', (req, res) => {
         redirect: 'follow'
     };
 
-    fetch("https://api.finicity.com/aggregation/v2/customers/testing", requestOptions)
+    fetch("https://api.finicity.com/aggregation/v2/customers/active", requestOptions)
         .then(response => response.json())
         .then(result => res.status(200).send(result))
         .catch(error => res.status(500).json({ error: error }));
